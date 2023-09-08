5129d726be729d17681e04023c105d4d7f4011ab
Vasiliy Novikov <vanoviko@microsoft.com>
2023-09-08 17:14:15 +0300

    Empty app targeting Tizen & Windows

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace OneDrivePhotos;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OneDrivePhotos;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
